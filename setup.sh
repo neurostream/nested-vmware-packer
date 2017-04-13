942ecb7e08ba90428101b933d40f8d40b0c305a2
neurostream <neurostream@users.noreply.github.com>
2017-04-13 01:53:16 -0600

    initial commit of setup.sh for Darwin assist, can only skip first prompt for now

--- setup.sh
+++ setup.sh
@@ -15,8 +15,5 @@ sleep 1
 osascript <<EOD
   tell application "System Events"
     tell process "Install Command Line Developer Tools"
-      keystroke return
-      click button "Agree" of window "License Agreement"
-    end tell
   end tell
 EOD
