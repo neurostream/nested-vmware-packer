1d2ddad907ea5a844a12aa634a1a27cd2f83946e
neurostream <neurostream@users.noreply.github.com>
2017-04-13 03:20:34 -0600

    initial commit of setup.sh for Darwin assist, attempt vagrant install if not there

--- setup.sh
+++ setup.sh
@@ -10,10 +10,29 @@ then
 	exit 1
 fi
 
-xcode-select --install
-sleep 1
+
+
+if [[ ! xcode-select --print-path ]] 
+	then
+	if [[ ! git version ]]
+	then
+		echo "git is required, offering XCode CLI Dev Tools. . ."
+	xcode-select --install
+	sleep 1
 osascript <<EOD
   tell application "System Events"
     tell process "Install Command Line Developer Tools"
   end tell
 EOD
+	else
+		echo "git appears to be installed - but not the version supplied with the XCode CLI Dev Tools "
+	fi
+fi
+
+if [[ ! vagrant version ]]
+then
+	hashiMirror="https://releases.hashicorp.com"
+	vagrantURI=$(curl -s ${hashiMirror}"$(curl -s ${hashiMirror}/vagrant/ | grep -E "/vagrant/[0-9]" | sort -n | tail -1 | awk -F '\"' '{print $2}')" | grep -i dmg | awk -F 'href=' '{print $2}' | awk -F '"' '{print $2}')
+	echo "vagrant was not found in the PATH. It can be downloaded from ${hashiMirror}${vagrantURI}"
+	curl -LRO ${hashiMirror}${vagrantURI} && open disk://$(basename ${vagrantURI})
+fi
