4af3d7e09a17676e09f5ca7c1f81f2d18ccfb9ff
neurostream <neurostream@users.noreply.github.com>
2017-05-04 04:08:17 -0600

    openvswitch build, export rpms after - not before

--- packer/build.sh
+++ packer/build.sh
@@ -6,5 +6,5 @@ rm -vrf packer_cache CentOS-7-x86_64-Minimal-1611*box output-vmware-iso *-iso.ov
 
 time VERSION=v${datestamp} ISOBASENAME=CentOS-7-x86_64-Minimal-1611 packer build -force -only vmware-iso CentOS_7.json
 
-unalias cp
-cp -f -v CentOS*vmware* /openshare/artifacts/
+
+test -d /openshare/artifacts && cp -f -v CentOS*vmware* /openshare/artifacts/
