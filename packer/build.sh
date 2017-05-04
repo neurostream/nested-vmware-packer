3bb5eb40e51b4d3e9681d5fa2c6853bd8f1ff100
neurostream <neurostream@users.noreply.github.com>
2017-05-04 03:42:58 -0600

    openvswitch build, download rpm built inside packer target instance to build home

--- packer/build.sh
+++ packer/build.sh
@@ -4,5 +4,7 @@ datestamp=$(date "+%Y%m%d%H%M%S")
 
 rm -vrf packer_cache CentOS-7-x86_64-Minimal-1611*box output-vmware-iso *-iso.ova
 
-time VERSION=v${datestamp} PACKER_LOG=1 ISOBASENAME=CentOS-7-x86_64-Minimal-1611 packer build -force -only vmware-iso CentOS_7.json
+time VERSION=v${datestamp} ISOBASENAME=CentOS-7-x86_64-Minimal-1611 packer build -force -only vmware-iso CentOS_7.json
 
+unalias cp
+cp -f -v CentOS*vmware* /openshare/artifacts/
