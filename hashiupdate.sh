0f53350a140e24274fe3c7e50f40f29062566604
neurostream <alangourley@gmail.com>
2017-05-09 18:12:32 -0600

    download for local os (don't assume linux)
    download for local os name using "uname -s"

--- hashiupdate.sh
+++ hashiupdate.sh
@@ -3,10 +3,11 @@
 # usage
 # hashiupdate.sh vagrant
 # example here is vagrant. can be consul, vault, etc. - defaults to packer
+localosname=$(uname | awk '{print tolower($1)}')
 hashiproduct=${1:-packer}
 hashimirror=https://releases.hashicorp.com
 hashilatest=$(curl -skRL ${hashimirror}/${hashiproduct}/ | grep ${hashiproduct}_ | awk -F '>' '$0~/href=/{print $2}' | awk -F '<' '{print $1}' | sort -n | grep -v "\-rc" | tail -1 | awk -F '_' '{print $2}')
-hashilatesturi=$(curl -skRL ${hashimirror}/${hashiproduct}/${hashilatest}/ | grep -iE "centos|linux" | grep -i 64 | awk -F 'href=' '{print $2}' | awk -F \" '{print $2}')
+hashilatesturi=$(curl -skRL ${hashimirror}/${hashiproduct}/${hashilatest}/ | grep -iE "${localosname}" | grep -i 64 | awk -F 'href=' '{print $2}' | awk -F \" '{print $2}')
 hashilatesturl=${hashimirror}${hashilatesturi}
 echo ${hashilatesturl}
 curl -skROL ${hashilatesturl} && filename=$(basename ${hashilatesturi}) && echo downloaded ${filename}
